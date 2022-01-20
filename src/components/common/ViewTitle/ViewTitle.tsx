b6cc1bdf997cca14dcb5a5248a475629faa8628e
vvelc <victorvelazquezcid@gmail.com>
2022-01-20 15:04:21 -0400

    Create ViewTitle Component

---
+++ src/components/common/ViewTitle/ViewTitle.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {};
+
+function ViewTitle({}: Props) {
+  return (
+    <React.Fragment>
+        <div>ViewTitle Works!</div>
+    </React.Fragment>
+  );
+}
+
+export default ViewTitle;
