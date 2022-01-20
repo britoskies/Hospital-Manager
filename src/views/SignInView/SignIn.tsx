a0860a5fae8e244e3033aa971ae001e9e57d2f85
vvelc <victorvelazquezcid@gmail.com>
2022-01-20 14:59:57 -0400

    Create Sign In View

---
+++ src/views/SignInView/SignIn.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {};
+
+function SignInView({}: Props) {
+  return (
+    <React.Fragment>
+        <div>SignInView Works!</div>
+    </React.Fragment>
+  );
+}
+
+export default SignInView;
