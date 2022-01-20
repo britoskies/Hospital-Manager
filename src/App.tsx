a5a7d84d2b04ec81f749b26e2d5d51118af4d25b
vvelc <victorvelazquezcid@gmail.com>
2022-01-20 15:56:12 -0400

    UserAuth testing in app component

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,32 @@ import Routes from './routes/Routes';
 // firebase imports
 import app from './services/firebase/firebase'
 
+import UserAuth from './models/userauth/UserAuth'
+
 type Props = {};
 
 function App({ }: Props): ReactElement {
-  
+  const auth = () => {
+    UserAuth.authUser('victor@gmail.com', '12345678')
+  }
+
+  const [value, loading, error] = UserAuth.getAuthState()
+
+  const verify = () => {
+    console.log(value);
+  }
+
+  const signout = () => {
+    UserAuth.signOut()
+  }
+
   return (
     <React.Fragment>
+      <input type="email" placeholder="email" />
+      <input type="password" placeholder='pass' />
+      <button onClick={() => auth()}>SignIn</button>
+      <button onClick={() => verify()}>Verify</button>
+      <button onClick={() => signout()}>SignOut</button>
       <BrowserRouter>
         <Routes/>
       </BrowserRouter>
