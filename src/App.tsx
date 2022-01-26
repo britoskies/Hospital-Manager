bd12662fdd3b17dd0f0d20f8d7988585e62ec508
vvelc <victorvelazquezcid@gmail.com>
2022-01-26 19:39:59 -0400

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
 // React imports
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
 
 // Roting imports
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes/Routes';
-
-// firebase imports
-import app from './services/firebase/firebase'
-import UserAuth from './models/userauth/UserAuth'
-
-// firestore imports
-import PatientModel from './models/patient/PatientModel';
-import ApptModel from './models/appointments/ApptModel';
-import { Timestamp } from 'firebase/firestore';
 
 type Props = {};
 
