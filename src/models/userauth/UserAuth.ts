b8d6d24daabf38b73b717a6bb64fda3a14c61523
britoskies <britoskies@gmail.com>
2022-01-29 15:48:38 -0400

    Moved Signin logic to a new component

--- src/models/userauth/UserAuth.ts
+++ src/models/userauth/UserAuth.ts
@@ -1,15 +1,27 @@
 // Firebase App
 import app from '../../services/firebase/firebase';
 
+// Firebase Classes
+import { FirebaseError } from 'firebase/app'
+
 // Auth Hooks
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword, UserCredential } from 'firebase/auth';
+
 
 const UserAuth = {
     _auth: getAuth(app),
 
     async authUser(email: string, password: string) {
-        signInWithEmailAndPassword(this._auth, email, password)
+        const result: UserCredential | FirebaseError | any = await signInWithEmailAndPassword(this._auth, email, password)
+            .then((res: UserCredential) => res)
+            .catch((err: FirebaseError) => err);
+
+        if (result.user) {
+            return true
+        } else {
+            return false
+        }
     },
 
     getAuthState() {
