fcf7f1d3ab08d707459b8d0cbaedb682df0e9729
vvelc <victorvelazquezcid@gmail.com>
2022-01-31 16:26:56 -0400

    Appointments Components

--- src/views/AppointmentsView/AppointmentsView.tsx
+++ src/views/AppointmentsView/AppointmentsView.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+// React imports
+import React, { useState } from 'react';
+
+// Mui Components
+import { 
+  Button
+} from '@mui/material';
+
+// Appointments Components
+import AddDialog from '../../components/Appointments/AddDialog';
+import Table from '../../components/Appointments/Table';
 
 type Props = {};
 
 function AppointmentsView({}: Props) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <React.Fragment>
         <div>AppointmentsView Works!</div>
+        <Button variant="contained" onClick={handleClickOpen}>Add Appointment</Button>
+        <AddDialog
+          open={open}
+          onClose={handleClose}
+        />
+        <Table />
     </React.Fragment>
   );
 }
