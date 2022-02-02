89d6c1678ee5345a4ebcfab0bda0a97e8881249d
vvelc <victorvelazquezcid@gmail.com>
2022-02-01 20:14:35 -0400

    Tried to fix guard but decided to change routes

--- src/guards/routeGuard.ts
+++ src/guards/routeGuard.ts
@@ -3,7 +3,14 @@ import UserAuth from "../models/userauth/UserAuth"
 
 function routeGuard() {
     const [user, loading, error] = UserAuth.getAuthState()
-    return user
+    // if(!loading) {
+    //     console.log('ended loading', user)
+    //     return user ? true : false
+    // }
+    //console.log('loading...', user)
+
+    return [user, loading]
+
 }
 
 export default routeGuard
