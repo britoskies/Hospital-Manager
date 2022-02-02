046623b26701d80d914efdd318581ef945f2609d
vvelc <victorvelazquezcid@gmail.com>
2022-02-01 20:11:44 -0400

    Make Functional Header Component

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,9 +1,59 @@
 import React from 'react';
 
+import {AppBar, Toolbar, IconButton, Typography, Box} from '@mui/material'
+
+import {Menu as MenuIcon, AccountCircle, Logout as Logout} from '@mui/icons-material'
+import UserAuth from '../../../models/userauth/UserAuth';
+
+import Logo from '../../../assets/img/logo.png'
+import { useNavigate } from 'react-router-dom';
+
 type Props = {};
 
 function Header({}: Props) {
-  return <div>Header Works!</div>;
+  const [user, loading, error] = UserAuth.getAuthState()
+  const navigate = useNavigate()
+
+  const handleSignOut = () => {
+    UserAuth.signOut()
+    navigate('/login')
+  }
+
+  const appTitle = "Hospital Manager"
+
+  return (
+    <AppBar position="static" color="inherit">
+        
+          {user && (
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              {appTitle}
+              </Typography>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleSignOut}
+                color="inherit"
+              >
+                <Logout />
+              </IconButton>
+            </Toolbar>
+          )}
+          {!user && (
+            <Toolbar sx={{display: 'flex', justifyContent: 'center'}}>
+              <Box sx={{display: 'flex', alignItems: 'center', flexDirection: 'row'}}>
+                <img src={Logo} alt="" height="45"/>
+                <Typography variant="h6" component="h6" sx={{ height: 'min-content' }}>
+                  {appTitle}
+                </Typography>
+              </Box>
+            </Toolbar>
+          )}
+        
+      </AppBar>
+  );
 }
 
 export default Header;
