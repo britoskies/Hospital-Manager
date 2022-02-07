ddc0e8f6963324f9fd8247bc3e2c64aee965db8b
vvelc <victorvelazquezcid@gmail.com>
2022-02-07 17:00:02 -0400

    Added Icons and Styles

--- src/components/common/SideBar/SideBar.tsx
+++ src/components/common/SideBar/SideBar.tsx
@@ -1,19 +1,54 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Box } from "@mui/material";
+import { Link, useLocation } from "react-router-dom";
+import {
+  Home as HomeIcon,
+  CalendarToday as CalendarIcon,
+} from "@mui/icons-material";
+import { mdiHome, mdiAccountClock, mdiCalendarBlank } from "@mdi/js";
+import { Icon } from "@mdi/react";
 
 type Props = {};
 
-function SideBar({}: Props) {
+import "./SideBar.css";
+
+function SideBar({ }: Props) {
+  const location = useLocation();
+
+  const activeRoute: string = location.pathname.replace('/', '')
+
   return (
-    <React.Fragment>
-        <div>SideBar Works!</div>
-        <ul>
-            <li><Link to="/dashboard">Go to Dashboard</Link></li>
-            <li><Link to="/patients">Go to Patients</Link></li>
-            <li><Link to="/patient/123">Go to a Certain Patient</Link></li>
-            <li><Link to="/appointments">Go to Appointments</Link></li>
-        </ul>
-    </React.Fragment>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+        bgcolor: '#1D3557',
+        mr: 3,
+        p: 3
+      }}
+      className="sidebar">
+      <Box sx={{position: 'relative', height: '48px', width: '24px', mb: '20px'}} className="sidebar-img-container">
+        <img src="/src/assets/img/logo.png" height="40" className="sidebar-img"/>
+      </Box>
+      <ul className="sidebar-icon-list">
+        <li className="sidebar-icon">
+          <Link to="/dashboard" className={`sidebar-icon ${activeRoute == "dashboard" ? "active":""}`}>
+            <Icon path={mdiHome} size={1.3} />
+          </Link>
+        </li>
+        <li className="sidebar-icon">
+          <Link to="/patients" className={`sidebar-icon ${activeRoute == "patients" ? "active":""}`}>
+            <Icon path={mdiAccountClock} size={1.3} />
+          </Link>
+        </li>
+        <li className="sidebar-icon">
+          <Link to="/appointments" className={`sidebar-icon ${activeRoute == "appointments" ? "active":""}`}>
+            <Icon path={mdiCalendarBlank} size={1.3} />
+          </Link>
+        </li>
+      </ul>
+    </Box>
   );
 }
 
